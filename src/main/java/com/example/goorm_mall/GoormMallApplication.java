86ca3ce349fa066e3aabda8513cc4e9f8effa272
carbancle <carbancle2@gmail.com>
2024-08-12 09:46:15 +0900

    first commit

---
+++ src/main/java/com/example/goorm_mall/GoormMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.goorm_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goorm_mall/GoormMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goorm_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
